69d28bccb767bba303fb1fefd7b9f0ee29c6d596
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-05-11 23:42:48 +0300

    function forEach was used

---
+++ index.js
@@ -0,0 +1,8 @@
+function points(games) {
+    let count = 0
+    games.forEach(item => {
+        if (item[0] > item [2]) count += 3;
+        if (item[0] === item[2]) count +=1;
+    })
+    return count;
+}
